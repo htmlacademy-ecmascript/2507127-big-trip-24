2ab80a008425d9136b632637f7b670780df9b6f2
Farit <afroid13@gmail.com>
2024-09-01 21:11:49 +0400

    Изменения в формах создания/редактирования

--- src/view/form-add-event-view.js
+++ src/view/form-add-event-view.js
@@ -1,34 +1,27 @@
 import { createElement } from '../render.js';
-import { increaseNumber } from '../utils.js';
 import { EVENT_OFFERS, PHOTOS_AMOUNT, EVENT_TYPE } from '../const.js';
 
-//Замыкание для создания id с другим номером
-const increment = increaseNumber();
 
-function createEventTypeItemTemplate(type, num) {
+function createEventTypeItemTemplate(type) {
   return `
     <div class="event__type-item">
-      <input id="event-type-${type}-${num}" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
-      <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-${num}">${type}</label>
+      <input id="event-type-${type}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${type}">
+      <label class="event__type-label  event__type-label--${type}" for="event-type-${type}-1">${type}</label>
     </div>
   `;
 }
 
-const addEventTypeList = () => {
-  const num = increment();
-  return EVENT_TYPE.map((type) => createEventTypeItemTemplate(type, num)).join('');
-};
+const addEventTypeList = () => EVENT_TYPE.map((type) => createEventTypeItemTemplate(type)).join('');
 
 function createFormHeaderTemplate() {
-  const num = increment();
   return `
     <header class="event__header">
                   <div class="event__type-wrapper">
-                    <label class="event__type  event__type-btn" for="event-type-toggle-${num}">
+                    <label class="event__type  event__type-btn" for="event-type-toggle-1">
                       <span class="visually-hidden">Choose event type</span>
                       <img class="event__type-icon" width="17" height="17" src="img/icons/flight.png" alt="Event type icon">
                     </label>
-                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${num}" type="checkbox">
+                    <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
 
                     <div class="event__type-list">
                       <fieldset class="event__type-group">
@@ -40,11 +33,11 @@ function createFormHeaderTemplate() {
                   </div>
 
                   <div class="event__field-group  event__field-group--destination">
-                    <label class="event__label  event__type-output" for="event-destination-${num}">
+                    <label class="event__label  event__type-output" for="event-destination-1">
                       Flight
                     </label>
-                    <input class="event__input  event__input--destination" id="event-destination-${num}" type="text" name="event-destination" value="Geneva" list="destination-list-1">
-                    <datalist id="destination-list-${num}">
+                    <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="Geneva" list="destination-list-1">
+                    <datalist id="destination-list-1">
                       <option value="Amsterdam"></option>
                       <option value="Geneva"></option>
                       <option value="Chamonix"></option>
@@ -52,19 +45,19 @@ function createFormHeaderTemplate() {
                   </div>
 
                   <div class="event__field-group  event__field-group--time">
-                    <label class="visually-hidden" for="event-start-time-${num}">From</label>
-                    <input class="event__input  event__input--time" id="event-start-time-${num}" type="text" name="event-start-time" value="19/03/19 00:00">
+                    <label class="visually-hidden" for="event-start-time-1">From</label>
+                    <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="19/03/19 00:00">
                     &mdash;
-                    <label class="visually-hidden" for="event-end-time-${num}">To</label>
-                    <input class="event__input  event__input--time" id="event-end-time-${num}" type="text" name="event-end-time" value="19/03/19 00:00">
+                    <label class="visually-hidden" for="event-end-time-1">To</label>
+                    <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="19/03/19 00:00">
                   </div>
 
                   <div class="event__field-group  event__field-group--price">
-                    <label class="event__label" for="event-price-${num}">
+                    <label class="event__label" for="event-price-1">
                       <span class="visually-hidden">Price</span>
                       &euro;
                     </label>
-                    <input class="event__input  event__input--price" id="event-price-${num}" type="text" name="event-price" value="">
+                    <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="">
                   </div>
 
                   <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -73,12 +66,12 @@ function createFormHeaderTemplate() {
   `;
 }
 
-function createEventOfferTemplate(offer, num){
+function createEventOfferTemplate(offer){
 
   return `
     <div class="event__offer-selector">
-                        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.name}-${num}" type="checkbox" name="event-offer-${offer.name}">
-                        <label class="event__offer-label" for="event-offer-${offer.name}-${num}">
+                        <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.name}-1" type="checkbox" name="event-offer-${offer.name}">
+                        <label class="event__offer-label" for="event-offer-${offer.name}-1">
                           <span class="event__offer-title">${offer.title}</span>
                           &plus;&euro;&nbsp;
                           <span class="event__offer-price">${offer.price}</span>
@@ -109,18 +102,16 @@ function createEventPhotoContainerTemplate(){
   `;
 }
 
-const addOfferList = () => {
-  const num = increment();
-  return EVENT_OFFERS.map((offer) => createEventOfferTemplate(offer, num)).join('');
-};
+const addOfferList = () => EVENT_OFFERS.map((offer) => createEventOfferTemplate(offer)).join('');
 
-function createEventDestinationTemplate(addPhoto = false) {
+
+function createEventDestinationTemplate() {
   return `
     <section class="event__section  event__section--destination">
                     <h3 class="event__section-title  event__section-title--destination">Destination</h3>
                     <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
 
-                    ${addPhoto ? createEventPhotoContainerTemplate() : ''}
+                    ${createEventPhotoContainerTemplate()}
                   </section>
   `;
 }
@@ -167,5 +158,3 @@ export default class FormAddEventView{
     this.element = null;
   }
 }
-
-export {addOfferList, createEventDetailsTemplate, createEventDestinationTemplate, addEventTypeList, increment };
